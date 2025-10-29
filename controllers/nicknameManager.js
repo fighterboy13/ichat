5a6436b71773a76d1ee43ee5c21a03d37e155048
fighterboy13 <dhanajyagupta@gmail.com>
2025-10-29 07:00:46 +0530

    Update nicknameManager.js

--- controllers/nicknameManager.js
+++ controllers/nicknameManager.js
@@ -1,15 +1,13 @@
+// controllers/nicknameManager.js
 import { log } from '../utils/logger.js';
 
-export async function setNickname(ig, thread, userId, nickname) {
+export async function setNickname(ig, threadId, userId, nickname) {
   try {
-    // instagram-private-api does not have a stable method to set chat nicknames for other users.
-    // This is a placeholder to show where nickname logic would go.
-    log('setNickname called for', userId, '->', nickname);
-    // Inform thread
-    if (ig.directThread?.broadcastText) {
-      await ig.directThread.broadcastText(thread.thread_id, `Nickname for ${userId} set to ${nickname}`);
-    }
-  } catch (err) {
-    log('setNickname error:', err.message);
+    // instagram-private-api does not reliably provide nickname set for other users.
+    // This is a placeholder: we inform the thread.
+    await ig.directThread.broadcastText(threadId, `Nickname for ${userId} set to ${nickname}`);
+    log('setNickname called:', userId, nickname);
+  } catch (e) {
+    log('setNickname error:', e.message || e);
   }
 }
